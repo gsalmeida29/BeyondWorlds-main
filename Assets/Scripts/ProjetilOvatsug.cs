6edd7cfe116bccc4352bb158917c34dd42f77753
Gustavo <gustavoearthursantosss@gmail.com>
2026-09-16 14:30:46 -0300

    FimProjetilOvatsug

--- Assets/Scripts/ProjetilOvatsug.cs
+++ Assets/Scripts/ProjetilOvatsug.cs
@@ -1,18 +1,29 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+using System;
 
-public class ProjetilOvatsug : MonoBehaviour
+class Program
 {
-    // Start is called before the first frame update
-    void Start()
+    static void Main()
     {
-        
-    }
+        double velocidade = 20.0; // m/s
+        double anguloGraus = 45.0;
+        double gravidade = 9.81;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        double angulo = anguloGraus * Math.PI / 180.0;
+
+        double vx = velocidade * Math.Cos(angulo);
+        double vy = velocidade * Math.Sin(angulo);
+
+        Console.WriteLine("Tempo | X | Y");
+
+        for (double t = 0; t <= 3; t += 0.1)
+        {
+            double x = vx * t;
+            double y = vy * t - 0.5 * gravidade * t * t;
+
+            if (y < 0)
+                break;
+
+            Console.WriteLine($"{t:F1}s | {x:F2}m | {y:F2}m");
+        }
     }
 }
