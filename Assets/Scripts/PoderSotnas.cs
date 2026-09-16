fbaac6c0b927ac06fe23a77ae8ac9327b53f6008
Gustavo <gustavoearthursantosss@gmail.com>
2026-09-16 14:46:15 -0300

    Poder personagem sotnas

--- Assets/Scripts/PoderSotnas.cs
+++ Assets/Scripts/PoderSotnas.cs
@@ -2,17 +2,35 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public float velocidade = 10f;
+
+
 public class PoderSotnas : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
+     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            Atacar();
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    void Atacar()
     {
-        
+        GameObject novoProjetil = Instantiate(
+            projetil,
+            pontoDeDisparo.position,
+            Quaternion.identity
+        );
+
+        Rigidbody2D rb = novoProjetil.GetComponent<Rigidbody2D>();
+
+        float direcao = transform.localScale.x;
+
+        rb.linearVelocity = new Vector2(
+            direcao * velocidade,
+            0
+        );
     }
 }
